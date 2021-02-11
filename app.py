bb8bf9fbe26b28ab3990191407397836c1be38b4
debarati-datta <debarati.datta@outlook.com>
2021-02-11 04:17:03 +0000

    Delete Recipe from Database

--- app.py
+++ app.py
@@ -132,6 +132,13 @@ def edit_recipe(recipe_id):
     return render_template("edit_recipe.html", recipe=recipe, catagories=catagories)
 
 
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Task Successfully Deleted")
+    return redirect(url_for("get_recipes"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
