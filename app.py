b359d9c25f5712a0eaadbf4222b8b7ca9e9cfa79
debarati-datta <debarati.datta@outlook.com>
2021-02-15 07:07:11 +0000

    Add recipe video to Recipes template

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def login():
         existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
 
         if existing_user:
-        # check if hashed password matches user input
+            # check if hashed password matches user input
             if check_password_hash(existing_user["password"], 
             request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
